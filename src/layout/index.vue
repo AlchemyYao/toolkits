966839e5d23612be88166708425eb3a3397524ab
yaoyafeng <yaoyafeng1206@163.com>
2022-11-08 00:15:49 +0800

    add layout

---
+++ src/layout/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-container>
+        <el-container>
+            <el-aside>Aside</el-aside>
+            <el-main>Main</el-main>
+        </el-container>
+        <el-footer></el-footer>
+    </el-container>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+<style scoped lang="scss">
+.el-container {
+    height: 100%;
+}
+.el-footer {
+    background-color: rgb(77, 77, 75);
+    height: 20px;
+}
+</style>
