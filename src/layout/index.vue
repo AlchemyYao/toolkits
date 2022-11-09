49d650c21924ffd5b1d8fa213d4491f3f97c0bad
yaoyafeng <yaoyafeng1206@163.com>
2022-11-10 00:29:28 +0800

    add iconfont and alibba font

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,10 +1,11 @@
 <template>
     <el-container>
         <el-container>
-            <el-aside>Aside</el-aside>
-            <el-main>Main</el-main>
+            <el-aside style="background-color:yellow" id="layout-aside">Aside</el-aside>
+            <el-aside style="background-color:gray">A</el-aside>
+            <el-main style="background-color:blue">Main</el-main>
         </el-container>
-        <el-footer></el-footer>
+        <el-footer style="background-color:green"></el-footer>
     </el-container>
 </template>
 
@@ -18,5 +19,8 @@ import { RouterView } from 'vue-router'
 .el-footer {
     background-color: rgb(77, 77, 75);
     height: 20px;
+}
+#layout-aside {
+    width: 55px;
 }
 </style>
